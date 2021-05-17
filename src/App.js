8472728e524fd5e45b77e26f5208b3496a54100b
SiUAhn <ahngo13@naver.com>
2021-05-17 10:56:36 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, AreaChart, Area } from 'recharts';
 
 function App() {
+  const data = [{name: 'Page A', uv: 0, pv: 100, amt: 2400}, {name: 'Page B', uv: 200, pv: 210, amt: 2400}, {name: 'Page C', uv: 300, pv: 210, amt: 2400}];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LineChart width={400} height={400} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+        <Line dataKey="uv" stroke="#8884d8" />
+        <Line dataKey="pv" stroke="#ff0000" />
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5"/>
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip/>
+      </LineChart>
+
+      <AreaChart
+        data={data}
+        width={400} height={400}
+        margin={{
+          top: 10,
+          right: 30,
+          left: 0,
+          bottom: 0,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Area dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+      </AreaChart>
     </div>
   );
 }
